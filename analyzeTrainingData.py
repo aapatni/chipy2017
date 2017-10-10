a8d5f2e22ecd185782f5b004b204b63da77f6e38
Adam Patni <adampatni27@gmail.com>
2017-10-10 09:32:08 -0500

    Update analyzeTrainingData.py

--- analyzeTrainingData.py
+++ analyzeTrainingData.py
@@ -6,8 +6,7 @@ y=[]
 with open('training.csv') as csvfile:
     reader = csv.reader(csvfile,delimiter=' ')
     for row in reader:
-        print(row)
         x.append([row[0],row[1],row[2],row[3],row[4]])
         y.append(row[5])
-clf = MLPClassifier(solver='lbfgs',alpha=1e-5,hidden_layer_sizes=(15,),random_state=1)
+clf = MLPClassifier(solver='lbfgs',alpha=1e-5,hidden_layer_sizes=(5,2),random_state=1) #solver could also equal 'adam', or 'sgd'
 print (clf.fit(x,y))
